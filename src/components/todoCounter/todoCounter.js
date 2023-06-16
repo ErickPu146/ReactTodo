c0e6057922dfb89bd5015495eb6b782de452ca06
Erick Pu Ramírez <erirami.gt33@gmail.com>
2023-06-15 20:55:42 -0600

    Arreglo todoCounter

--- src/components/todoCounter/todoCounter.js
+++ src/components/todoCounter/todoCounter.js
@@ -1,8 +1,16 @@
 function TodoCounter({ total, completed }) {
     return (
         <div className="fs-1">
-          Haz completado <span className="fw-bold"> {completed} </span> de 
-          <span className="fw-bold"> {total} </span> TODOs
+          <span className={`${total == 0 | total == completed ? 'd-none' : 'd-block'}`}>
+            Haz completado <span className="fw-bold"> {completed} </span> de 
+            <span className="fw-bold"> {total} </span> tarea's
+          </span>
+          <span className={`${total > 0 & total == completed ? 'd-block' : 'd-none'} fw-bold`}>
+            Felcidades, haz completado todas tus tareas :)
+          </span>
+          <span className={`${total == 0 ? 'd-block' : 'd-none'} fw-bold`}>
+            Agrega Tareas
+          </span>
         </div>
       )
 }
